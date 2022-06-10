754cdf72ca8b147dc43f1acc31e4f0b70cc20470
sayonara312 <phantrong321@gmail.com>
2022-06-10 11:43:33 +0700

    apitiktok

--- src/pages/Home/components/ContentItem/index.js
+++ src/pages/Home/components/ContentItem/index.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle, faMusic } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faComment, faHeart, faMusic, faShare } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
 import Button from '~/components/Button';
 import styles from './ContentItem.module.scss';
@@ -25,20 +25,49 @@ function ContentItem({ data }) {
         </div>
         <div className={cx('title')}>
           He doesn’t want to be my friend 😢🥺 (I never give up) see you soon in 🇮🇩!!……to be continued.
+          <Link to="/" className={cx('tag')}>
+            <span>#learnfromkhaby</span>
+          </Link>
+          <Link to="/" className={cx('tag')}>
+            <span>#tiktok</span>
+          </Link>
+          <Link to="/" className={cx('tag')}>
+            <span> #learnontiktok</span>
+          </Link>
         </div>
-        <div className={cx('tag')}>#learnfromkhaby #learnontiktok @tiktok</div>
+
         <div className={cx('music')}>
           <FontAwesomeIcon icon={faMusic} />
-          <span className={cx('music-name')}>suono originale - Khabane lame</span>
+          <Link to="/" className={cx('tag')}>
+            <span className={cx('music-name')}>suono originale - Khabane lame</span>
+          </Link>
         </div>
-        <div className={cx('video')}>
+
+        <div className={cx('main-content')}>
           <video
-            width="258"
-            heigt="462"
-            autoPlay
             controls
             src="https://v16-webapp.tiktok.com/a4931bfe03d9b8ca9b1d676923e74a02/62a2ff56/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/49a21c68917448818994ce5c31ac0906/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=3266&bt=1633&btag=80000&cs=0&ds=3&ft=eXd.6Hk_Myq8ZTEeawe2N1Rjml7Gb&mime_type=video_mp4&qs=0&rc=OTRmNmdlM2Q1PDM8OWU8Z0BpMzNlZGc6ZnE2PDMzZjczM0A1Nl5iMjQ0NS4xYDJjMmFgYSMybG5qcjRfbmRgLS1kMWNzcw%3D%3D&l=202206100221480102451551102268D1A0"
           ></video>
+          <div className={cx('action')}>
+            <div className={cx('follow')}>
+              <Button className={cx('btn-follow')}>
+                <FontAwesomeIcon icon={faHeart} />
+              </Button>
+              <span className={cx('number')}>904k</span>
+            </div>
+            <div className={cx('comment')}>
+              <Button className={cx('btn-comment')}>
+                <FontAwesomeIcon icon={faComment} />
+              </Button>
+              <span className={cx('number')}>10k</span>
+            </div>
+            <div className={cx('share')}>
+              <Button className={cx('btn-share')}>
+                <FontAwesomeIcon icon={faShare} />
+              </Button>
+              <span className={cx('number')}>1214</span>
+            </div>
+          </div>
         </div>
       </div>
       <div className={cx('followBtn')}>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,23 @@
 import classNames from 'classnames/bind';
 import styles from './Home.module.scss';
 import ContentItem from './components/ContentItem';
+import { useEffect } from 'react';
 const cx = classNames.bind(styles);
 function Home() {
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '71fc7976bbmshdcd4632c1a5c86ep1a5d80jsna1b4a8f8fcf8',
+        'X-RapidAPI-Host': 'tiktok82.p.rapidapi.com',
+      },
+    };
+
+    fetch('https://tiktok82.p.rapidapi.com/getChallengeVideos?challenge_id=229207', options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <div className={cx('wrapper')}>
       <ContentItem />
